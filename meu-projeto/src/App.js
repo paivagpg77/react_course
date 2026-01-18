fe0a5e6339201906cecdaf208d209771f4d0a890
paivagpg77 <paivag171@gmail.com>
2026-01-18 19:35:00 -0300

    css modules

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import SayMyName from './components/SayMyName';
 import Pessoa from './components/Pessoa';
+import Frase from './components/Frase';
 
 function App() {
   const nome = "Maria"
   return (
     <div className="App">
+      <h1>Testando CSS</h1>
+      <Frase/>
+      <Frase/>
       <SayMyName nome="Gabriel"/>
       <SayMyName nome={nome}/>
       <Pessoa nome="Gabriel" idade="15 anos" profissao="Programador"  foto="https://randomuser.me/api/portraits/men/32.jpg
